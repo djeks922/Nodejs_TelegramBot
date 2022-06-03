541704370e2b7724d97630bc7ab305ddf9422bf7
djeks922 <djeks922@gmail.com>
2022-06-03 15:32:13 +0400

    Paymen scenes for customer-admin and admin-influencer

---
+++ Scenes/Payment/consumer/index.js
@@ -0,0 +1,52 @@
+import { Scenes } from "telegraf";
+import {getAdmins} from '../../../api/service/consumer.js'
+import {createTransaction} from '../../../api/service/transaction.js'
+import {consumerPaymentText} from '../../../helpers/consumer.js'
+import { adminPaymentButtons } from "../markup.js";
+const { BaseScene } = Scenes
+
+
+const paymentConsumerScene = new BaseScene('payment-scene-toAdmin-id')
+
+paymentConsumerScene.enter(async( ctx) => {
+    console.log(ctx)
+    await ctx.reply('Please enter TaxID of transaction for related package(influencer)')
+    console.log(ctx.scene.state)
+    const proposal = ctx.session.proposals.find(p => `${p._id}` === ctx.scene.state.proposalID)
+    ctx.scene.state.proposal = proposal
+    proposal.packages.forEach(pkg => `${pkg._id}` === ctx.scene.state.pkgID ? ctx.scene.state.pkg = pkg : '')
+    // console.log(ctx.scene.state.pkg)
+})
+
+paymentConsumerScene.on('text', async(ctx) => {
+    try {
+        // console.log(ctx.scene.state)
+        const admin = await getAdmins()
+        const {proposal,pkg} = ctx.scene.state
+        
+        const transaction = {
+            txID: ctx.message?.text,
+            from: ctx.session.consumer._id,
+            to: admin._id,
+            onUser: 'tg-consumer',
+            proposal: proposal,
+            FOR: pkg
+        }
+        const tr = await createTransaction(transaction)
+        await ctx.telegram.sendMessage(admin.chatID, consumerPaymentText(ctx,proposal,pkg),adminPaymentButtons(tr._id,proposal._id))
+        await ctx.reply('Great, after TXID verification we`ll update you.')
+        await ctx.scene.leave()
+        return 
+    } catch (error) {
+        throw error
+    }
+})
+paymentConsumerScene.on('message', async(ctx) => {
+    try {
+        await ctx.reply('Please enter valid TaxID')
+    } catch (error) {
+        throw error
+    }
+})
+
+export default paymentConsumerScene
---
+++ Scenes/Payment/influencer/index.js
@@ -0,0 +1,70 @@
+import { Scenes,Composer } from "telegraf";
+import {createTransaction,getTransactionByID,getVerifiedTransactions} from '../../../api/service/transaction.js'
+// import {  } from '../../api/service/transaction.js';
+import {adminPaymentText} from '../../../helpers/consumer.js'
+import { influencerPaymentButtons } from "../markup.js";
+const { WizardScene } = Scenes
+
+
+const transactionSelection = new Composer()
+transactionSelection.hears(/[0-9]/, async (ctx)=> {
+    try {
+        console.log(ctx.message.text)
+        // console.log(ctx.scene.state
+        ctx.scene.state.transaction = ctx.scene.state.transactions[+ctx.message.text]
+        await ctx.reply('Please enter the TxID of transaction')
+        ctx.wizard.next()
+    } catch (error) {
+        throw error
+    }
+
+})
+const txIDSubmition = new Composer()
+txIDSubmition.on('text', async (ctx)=> {
+    console.log(ctx.scene.state.transaction)
+    try {
+        const transactionI = {
+            txID: ctx.message?.text,
+            from: ctx.scene.state.transaction.from,
+            to: ctx.scene.state.transaction.FOR.influencer._id,
+            onUser: 'tg-influencer',
+            proposal: ctx.scene.state.transaction.proposal,
+            FOR: ctx.scene.state.transaction.FOR
+        }
+        const tr = await createTransaction(transactionI)
+        await ctx.telegram.sendMessage(ctx.scene.state.transaction.FOR.influencer.chatID, adminPaymentText(transactionI),influencerPaymentButtons(tr._id))
+        await ctx.reply('Transaction succesfully sent to Influencer.')
+        await ctx.scene.leave()
+    } catch (error) {
+        throw error
+    }
+
+})
+
+const paymentAdminScene = new WizardScene('payment-scene-toInfluencer-id',transactionSelection,txIDSubmition)
+
+// paymentAdminScene.on('callback_query', (ctx) => 
+// })
+paymentAdminScene.enter(async( ctx) => {
+    try {
+        const transactions = await getVerifiedTransactions()
+        let text = `Enter transaction number to forward payment to influencer\n`
+        for(let [i,transaction] of transactions.entries()){
+          text = text.concat(`${i}. For token ${transaction.proposal.name}(${transaction.from.name}) and package ${transaction.FOR.package.name}\n`)
+        }
+        ctx.scene.state.transactions = transactions
+        await ctx.reply(text)
+    } catch (error) {
+        throw error
+    }
+})
+
+// paymentAdminScene.on('message', async(ctx) => {
+//     try {
+//         await ctx.reply('Please enter valid TaxID')
+//     } catch (error) {
+//         throw error
+//     }
+// })
+
+export default paymentAdminScene
