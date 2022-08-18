0dedbe84e753bfc274ac16128d3524dc5f9d9e60
djeks922 <djeks922@gmail.com>
2022-08-18 18:43:39 +0400

    error handling adjustments

--- api/controller/proposal.js
+++ api/controller/proposal.js
@@ -2,39 +2,40 @@ import bot from "../../config/bot.config.js";
 import { createProposal } from "../service/proposal.js";
 
 export const postProposal = async (req, res, next) => {
+  const proposal = req.body;
+  const { queryID, consumerChatID } = req.query;
   try {
     // console.log(req.files)
-    const proposal = req.body;
-    const { queryID, consumerChatID } = req.query;
+
     // console.log({proposal,queryID,consumerChatID})
     if (!queryID || !consumerChatID) {
-      return res.status(400).send({ message: "Bad request, queryID not included or consumer not found." });
+      return res
+        .status(400)
+        .send({
+          message: "Bad request, queryID not included or consumer not found.",
+        });
     }
     if (req.files) {
       const pImages = [];
       for (let file of req.files) {
-        pImages.push(
-          `${process.env.APP_URL}/${file.path.replace(/\\/g, "/")}`
-        );
+        pImages.push(`${process.env.APP_URL}/${file.path.replace(/\\/g, "/")}`);
       }
       proposal.pImages = pImages;
     }
     proposal.packages = proposal.packages.split(",");
     // console.log(proposal.packages);
-    const webappData = JSON.parse(proposal.filtered)
-    delete proposal.filtered
+    const webappData = JSON.parse(proposal.filtered);
+    delete proposal.filtered;
 
-    const prop = await createProposal(proposal, consumerChatID);
-    
-    let webappDataText = `Packages: \n`
-    let totalPrice = 0
-    for(let el of webappData){
-      totalPrice += parseInt(el.package.price)
-      webappDataText+= `Influencer: ${el.influencer.name}
+    let webappDataText = `Packages: \n`;
+    let totalPrice = 0;
+    for (let el of webappData) {
+      totalPrice += parseInt(el.package.price);
+      webappDataText += `Influencer: ${el.influencer.name}
 Package: ${el.package.name}
-Price: ${el.package.price}\n`
+Price: ${el.package.price}\n`;
     }
-    webappDataText+=`Total Price: ${totalPrice}`
+    webappDataText += `Total Price: ${totalPrice}`;
     /** ANSWERING WEBAPP QUERY TO CLOSE THE APP */
     await bot.telegram.answerWebAppQuery(queryID, {
       type: "article",
@@ -63,7 +64,7 @@ Additional Info:
     });
     // console.log(proposal.pImages)
     if (proposal.pImages && proposal.pImages.length >= 1) {
-      console.log(proposal.pImages,'IMAGES INCLUDES IN PROPOSAL')
+      console.log(proposal.pImages, "IMAGES INCLUDES IN PROPOSAL");
       const mediaArr = [];
       for (let imgFile of proposal.pImages) {
         mediaArr.push({
@@ -71,13 +72,20 @@ Additional Info:
           media: imgFile,
         });
       }
-      await bot.telegram.sendMessage(consumerChatID,'Proposal Images: ')
-      const mediaSentInfo = await bot.telegram.sendMediaGroup(consumerChatID, mediaArr);
+      await bot.telegram.sendMessage(consumerChatID, "Proposal Images: ");
+      const mediaSentInfo = await bot.telegram.sendMediaGroup(
+        consumerChatID,
+        mediaArr
+      );
       // console.log(mediaSentInfo)
     }
+    const prop = await createProposal(proposal, consumerChatID);
 
-    res.send({message:'ok'});
+    res.send({ message: "ok" });
   } catch (error) {
+    if(error.status === 400){
+      bot.telegram.sendMessage(consumerChatID, 'Proposal failed, query timeout, try again later.');
+    }
     next(error);
   }
 };
